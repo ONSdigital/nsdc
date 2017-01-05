d59aed42a8a6b14d028c5e030c5653b4c713a9e4
Steven Job <steven.job@indigoblue.co.uk>
2017-01-05 09:30:05 +0000

    Remove unused import

--- server/server.py
+++ server/server.py
@@ -8,7 +8,6 @@ from api.role import Role
 from api.supplier import Supplier
 from api.permission import Permission
 from api.user import User
-from data.user_session import UserSessionData
 
 base_endpoint = '/nsdc/v1.0/'
 
