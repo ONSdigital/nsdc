57f685eceb7e604c800145162bbd88dc1ff77628
Steven Job <steven.job@indigoblue.co.uk>
2017-01-11 10:25:33 +0000

    Add permissions for permissions module

--- client/src/app/permission/permission-routing.module.ts
+++ client/src/app/permission/permission-routing.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { UserPermissionsGuard } from '../user-permissions.guard';
 import { PermissionListComponent } from './permission-list.component';
 import { AddPermissionComponent } from './add/add-permission.component';
 import { EditPermissionComponent } from './edit/edit-permission.component';
 
 const routes: Routes = [
   {
+    canActivate: [UserPermissionsGuard],
     path: 'add',
-    component: AddPermissionComponent
+    component: AddPermissionComponent,
+    data: {
+      permission: 'ADD_PERMISSIONS'
+    }
   },
   {
+    canActivate: [UserPermissionsGuard],
     path: ':id',
-    component: EditPermissionComponent
+    component: EditPermissionComponent,
+    data: {
+      permission: 'EDIT_PERMISSIONS'
+    }
   },
   {
+    canActivate: [UserPermissionsGuard],
     path: '',
-    component: PermissionListComponent
+    component: PermissionListComponent,
+    data: {
+      permission: 'VIEW_PERMISSIONS'
+    }
   },
 ];
 
