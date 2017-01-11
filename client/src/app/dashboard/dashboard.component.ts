21903e562454ff16371e162c0d6f38c95bbd1c30
Steven Job <steven.job@indigoblue.co.uk>
2017-01-11 10:53:23 +0000

    Add permissions to dashboard

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login/login.service';
 import { Router } from '@angular/router';
+import { UserPermissionsService } from '../user-permissions.service';
 
 
 @Component({
   selector: 'dashboard',
   templateUrl : './dashboard.component.html'
 })
-export class DashboardComponent {
-    constructor(private loginService: LoginService, private router: Router) {}
+export class DashboardComponent implements OnInit {
 
-    showMenuItems() {
-        return this.loginService.isLoggedIn();
-    }
+  permissionShortNames: string[] = [];
+
+  ngOnInit() {
+    this.userPermissionsService.getUserPermissions()
+    .subscribe(permissions => {
+      this.permissionShortNames = permissions.map(permission => permission.short_name);
+    });
+  }
+
+  constructor(
+    private userPermissionsService: UserPermissionsService,
+    private router: Router
+  ) {}
+
+    canViewUsers() {
+    return this.permissionShortNames.includes('VIEW_USERS');
+  }
+
+  canViewPermissions() {
+    return this.permissionShortNames.includes('VIEW_PERMISSIONS');
+  }
+
+  canViewRoles() {
+    return this.permissionShortNames.includes('VIEW_ROLES');
+  }
+
+  canViewUpload() {
+    return this.permissionShortNames.includes('DATA_IMPORT');
+  }
+
+  canViewAudit() {
+    return this.permissionShortNames.includes('DATA_AUDIT');
+  }
+
+  canViewJourneys() {
+    return this.permissionShortNames.includes('VIEW_JOURNEYS');
+  }
+
 }
