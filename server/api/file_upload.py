eb4cad64be36a7f2e10ba52aa2a1cde358fdfebf
Steven Job <steven.job@indigoblue.co.uk>
2017-01-17 14:09:45 +0000

    Check for journey validator on file upload

--- server/api/file_upload.py
+++ server/api/file_upload.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import jsonify, abort, request
 from flask_restful import Resource
 from protected_resource import protected_resource
@@ -6,42 +7,55 @@ from werkzeug.utils import secure_filename
 from config import app
 from data.file import FileData
 from data.file_journey_audit import FileJourneyAuditData
+from data.journey import JourneyData
 from config import db
 
 
-
-def write_file_status(filename, status, description):
-    db.session.add(FileData(filename))
+def write_file_status(filename, status, description, journey_id):
+    db.session.add(FileData(filename, journey_id))
     db.session.add(FileJourneyAuditData("UPLOAD_TO_SERVER", filename, status, description))
     db.session.commit()
+
+
+def validate_filename(filename, validator):
+    # add start and end to regex and replace * with .*
+    validator_regex = '^' + validator.replace('*', '.*') + '$'
+    match_found = re.match(validator_regex, filename)
+    return match_found is not None
 
 
 class FileUpload(Resource):
     @protected_resource('DATA_IMPORT')
     def post(self):
-        # check if the post request has the file part
         if 'file' not in request.files:
-            return abort(400, 'No file uploaded')
+            abort(400, 'No file uploaded')
         uploaded_file = request.files['file']
 
         if uploaded_file.filename == '':
-            error_message = 'Invalid filename'
-            return abort(400, error_message)
+            abort(400, 'Invalid filename')
 
         filename = secure_filename(uploaded_file.filename)
-        file_exists = FileJourneyAuditData.query\
+
+        journeys = JourneyData.query.all()
+        journey = next((journey for journey in journeys if validate_filename(filename, journey.validator)), None)
+
+        if journey is None:
+            abort(400, 'File has no journey')
+        journey_id = journey.id
+
+        file_exists = FileJourneyAuditData.query \
             .filter(FileJourneyAuditData.filename == filename, FileJourneyAuditData.status == 'success').count() > 0
 
         if file_exists:
             error_message = 'File with same name exists'
-            write_file_status(filename, 'error', error_message)
+            write_file_status(filename, 'error', error_message, journey_id)
             abort(400, error_message)
-        else:
-            try:
-                uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            except IOError:
-                error_message = 'Could not write file to folder'
-                write_file_status(filename, 'error', error_message)
-                return abort(400, error_message)
-        write_file_status(filename, 'success', 'Uploaded to server')
+
+        try:
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        except IOError:
+            error_message = 'Could not write file to folder'
+            write_file_status(filename, 'error', error_message, journey_id)
+            abort(400, error_message)
+        write_file_status(filename, 'success', 'Uploaded to server', journey_id)
         return jsonify(filename)
--- server/data/file.py
+++ server/data/file.py
@@ -6,12 +6,15 @@ class FileData(db.Model, Serializer):
     __tablename__ = 'file'
     id = db.Column('file_id', db.Integer, primary_key=True)
     name = db.Column('name', db.String(50), nullable=False, server_default=u'')
+    journey_id = db.Column(db.Integer, db.ForeignKey('journey.journey_id'))
 
-    def __init__(self, name):
+    def __init__(self, name, journey_id):
         self.name = name
+        self.journey_id = journey_id
 
     def serialize(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'journey_id': self.journey_id
         }
