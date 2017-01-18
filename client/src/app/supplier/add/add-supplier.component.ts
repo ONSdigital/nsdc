70731d19acb4665efbcfd639c93105f3bd56fd09
Steven Savery <steven.savery@indigoblue.co.uk>
2017-01-18 10:41:27 +0000

    Add Suppliers

---
+++ client/src/app/supplier/add/add-supplier.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Supplier } from '../supplier';
+import { SupplierService } from '../supplier.service';
+
+@Component({
+  selector: 'add-supplier',
+  templateUrl: 'add-supplier.component.html'
+})
+export class AddSupplierComponent implements OnInit {
+
+  supplierForm: FormGroup;
+  supplier: Supplier;
+  submitPending = false;
+  submitFailed = false;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private supplierService: SupplierService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.supplierForm = this._formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      description: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]]
+    });
+    this.supplier = new Supplier();
+  }
+
+  onSubmit() {
+    this.supplier.name = this.supplierForm.controls['name'].value;
+    this.supplier.description = this.supplierForm.controls['description'].value;
+    this.supplierService.addSupplier(this.supplier)
+    .subscribe(
+      () => {
+        this.submitPending = false;
+        this.router.navigate(['/suppliers']);
+      },
+      error => {
+        this.submitPending = false;
+        this.submitFailed = true;
+      }
+    );
+  }
+}
--- client/src/app/supplier/supplier-routing.module.ts
+++ client/src/app/supplier/supplier-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserPermissionsGuard } from '../user-permissions.guard';
+import { AddSupplierComponent } from './add/add-supplier.component';
 import { SupplierListComponent } from './supplier-list.component';
 
 const routes: Routes = [
+  {
+    canActivate: [UserPermissionsGuard],
+    path: 'add',
+    component: AddSupplierComponent,
+    data: {
+      permission: 'ADD_SUPPLIERS'
+    }
+  },
   {
     canActivate: [UserPermissionsGuard],
     path: '',
@@ -21,5 +30,6 @@ const routes: Routes = [
 export class SupplierRoutingModule { }
 
 export const routedComponents = [
-  SupplierListComponent
+  SupplierListComponent,
+  AddSupplierComponent
 ];
