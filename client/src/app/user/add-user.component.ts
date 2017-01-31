6389f71554e3c794e12b38112230864a5772e6bc
Steven Savery <steven.savery@indigoblue.co.uk>
2017-01-31 14:05:52 +0000

    User Forms Refactor

--- client/src/app/user/add-user.component.ts
+++ client/src/app/user/add-user.component.ts
@@ -4,7 +4,6 @@ import { Router } from '@angular/router';
 import { UserService } from './user.service';
 import { User } from './user';
 import { Role } from '../role/role';
-import { Configuration } from '../app.constants';
 import { RoleService } from '../role/role.service';
 
 @Component({
@@ -28,30 +27,30 @@ export class AddUserComponent implements OnInit {
 
   ngOnInit() {
     this.userForm = this._formBuilder.group({
-      firstname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      lastname: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
       email: [null, [Validators.required]],
-      username: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      password: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
       status: [],
-      role_id: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]]
+      role_id: [null, [Validators.required,]]
     });
+
     this.user = new User();
-    this.roleService.getRoles()
-    .then(roles => this.roles = roles);
+    this.roleService.getRoles().then(roles => this.roles = roles);
   }
 
 
   onSubmit() {
     this.submitFailed = false;
     this.submitPending = true;
-    this.user.username = this.userForm.controls['username'].value;
-    this.user.firstname = this.userForm.controls['firstname'].value;
-    this.user.lastname = this.userForm.controls['lastname'].value;
-    this.user.password = this.userForm.controls['password'].value;
-    this.user.email = this.userForm.controls['email'].value;
+
+    Object.keys(this.userForm.controls).forEach(key =>
+      this.user[key] = this.userForm.controls[key].value
+    );
+
     this.user.status = 'active';
-    this.user.role_id = this.userForm.controls['role_id'].value;
+
     this.userService.addUser(this.user)
     .subscribe(
       () => {
--- client/src/app/user/edit-user.component.ts
+++ client/src/app/user/edit-user.component.ts
@@ -12,7 +12,6 @@ import { RoleService } from '../role/role.service';
   templateUrl : 'user.component.html'
 })
 export class EditUserComponent implements OnInit {
-
   userForm: FormGroup;
   user: User;
   users: User [];
@@ -31,44 +30,31 @@ export class EditUserComponent implements OnInit {
 
   ngOnInit() {
     this.userForm = this._formBuilder.group({
-      firstname: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      lastname: [ null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      role_id: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      firstname: [null, [Validators.required]],
+      lastname: [ null, [Validators.required]],
+      role_id: [null, [Validators.required]],
       email: [null, [Validators.pattern('^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$')]],
-      username: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      password: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
       status: []
     });
 
-    this.roleService.getRoles()
-    .then(roles => {
-      this.roles = roles;
-    });
-
+    this.roleService.getRoles().then(roles => this.roles = roles);
     this.route.data.subscribe(data => {
       const user = data['user'];
       this.user = user;
-      this.userForm.patchValue({
-        username: user.username,
-        lastname: user.lastname,
-        password: user.password,
-        firstname: user.firstname,
-        email: user.email,
-        status: user.status,
-        role_id: user.role_id
-      });
+      this.userForm.patchValue(user);
     });
   }
 
   onSubmit() {
     this.submitFailed = false;
     this.submitPending = true;
-    this.user.username = this.userForm.controls['username'].value;
-    this.user.firstname = this.userForm.controls['firstname'].value;
-    this.user.lastname = this.userForm.controls['lastname'].value;
-    this.user.password = this.userForm.controls['password'].value;
-    this.user.email = this.userForm.controls['email'].value;
-    this.user.role_id = this.userForm.controls['role_id'].value;
+
+    Object.keys(this.userForm.controls).forEach(key =>
+      this.user[key] = this.userForm.controls[key].value
+    );
+
     this.userService.updateUser(this.user)
     .then(
       () => {
