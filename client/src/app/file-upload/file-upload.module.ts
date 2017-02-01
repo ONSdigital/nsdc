64fc96d6d85c65980dc287b8ff52cce79bad772d
Steven Job <steven.job@indigoblue.co.uk>
2017-02-01 15:55:18 +0000

    Update file upload

--- client/src/app/file-upload/file-upload.module.ts
+++ client/src/app/file-upload/file-upload.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingModule } from '../loading/loading.module';
-import { FileUploaderModule } from '../file-uploader/file-uploader.module';
+import { FileUploaderModule } from '../file-uploader';
 import { JourneyService } from '../journey/journey.service';
 import { ScheduleService } from '../schedule/schedule.service';
 import { FileUploadRoutingModule, routedComponents } from './file-upload-routing.module';
