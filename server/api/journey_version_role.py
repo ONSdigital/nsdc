a5b91a4f2cbfcd4092d69f5cfc8056e26982364b
Steven Job <steven.job@indigoblue.co.uk>
2017-02-02 16:27:29 +0000

    Hack away journey permissions

--- server/api/journey_version_role.py
+++ server/api/journey_version_role.py
@@ -7,7 +7,6 @@ from data.journey_version import JourneyVersionData
 
 class JourneyVersionRole(Resource):
 
-    @protected_resource('VIEW_JOURNEYS')
     def get(self, role_id):
         journey_versions = RoleData.query.get(role_id).journey_versions
         return jsonify(JourneyVersionData.serialize_list(journey_versions))
