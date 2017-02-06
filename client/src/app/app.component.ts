31624632f5136956aad7e88c0dfaf503a5be8b11
Steven Job <steven.job@indigoblue.co.uk>
2017-02-06 11:02:28 +0000

    Fix file endings and indent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nsdc-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {}
