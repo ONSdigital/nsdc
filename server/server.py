7aaa4ba628c2fc8af5c8fe16bc96e178fd66bbf1
Steven Job <steven.job@indigoblue.co.uk>
2017-02-07 13:39:05 +0000

    Add users endpoint on server

--- server/server.py
+++ server/server.py
@@ -29,6 +29,7 @@ api = Api(app)
 
 api.add_resource(
     User,
+    base_endpoint + 'users',
     base_endpoint + 'users/status/<status>',
     base_endpoint + 'users/<user_id>',
     base_endpoint + 'users/role/<role_id>',
