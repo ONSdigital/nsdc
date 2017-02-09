8e27d143c2da64701231d8f19d50aeaa7fe5db9d
Steven Savery <steven.savery@indigoblue.co.uk>
2017-02-09 14:07:02 +0000

    Updated Client Bundle

--- server/server.py
+++ server/server.py
@@ -147,5 +147,15 @@ api.add_resource(
     base_endpoint + 'upload'
 )
 
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, threaded=True)
