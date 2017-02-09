1d78c24135aee149f2d6a74aca929d845f68eb6a
Steven Job <steven.job@indigoblue.co.uk>
2017-02-09 08:25:15 +0000

    Remove scripts in root

--- av_clamscan.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def avcheck(filePath):
-	p = subprocess.Popen(['/home/cloudera/scripts/av_clamscan.sh', filePath], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-	# p = subprocess.Popen(['/home/cloudera/scripts/av_clamscan.sh', '/home/cloudera/landing', 'nsdc2.fix'], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-	out, err = p.communicate()
-	#print out
-	return out
